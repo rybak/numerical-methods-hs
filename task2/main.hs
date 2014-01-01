2952f426550a498fcc6e420153ee0f410be594c6
Andrey Rybak <rybak.a.v@gmail.com>
2014-01-02 01:33:38 +0400

    giant update

--- task2/main.hs
+++ task2/main.hs
@@ -8,78 +8,163 @@ import qualified Data.Vector as V
 import Text.Printf
 import Control.Monad
 
-import Graphics.EasyPlot
+-- gnuplot
+import qualified Graphics.Gnuplot.Advanced as GP
+import qualified Graphics.Gnuplot.Frame as Frame
+import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
+
+import qualified Graphics.Gnuplot.Graph as Graph
+
+import qualified Graphics.Gnuplot.Plot.ThreeDimensional as Plot3D
+import qualified Graphics.Gnuplot.Graph.ThreeDimensional as Graph3D
+
+-- /gnuplot
  
-dt, dx, u, kappa :: Float
-dt = 0.000001
-dx = 0.1
-u = 5000.0
-kappa = 300.0
-st, re :: Float
-st = (u * dt) / dx
-re = (kappa * dt) / (dx * dx)
+import System.Environment
+import System.Console.GetOpt
+
+type FPType = Double
+
 --re = kappa * dt / dx / dx
-x_left, x_right :: Float
+x_left, x_right :: FPType
 x_left = 1.0
 x_right = 0.0
-
 
 time_steps, space_steps :: Int
 time_steps = 200
 space_steps = 200
 
-initial :: Int -> Vector Float
+initial :: Int -> Vector FPType
 initial size = V.replicate (size `div` 2) 1.0 V.++ V.replicate (size - (size `div` 2)) 0.0
 
-type MethodType = Int -> Int -> [Vector Float]
-type EulerStep = Vector Float -> Vector Float
+data ParamsType = Container {kappa :: FPType, u :: FPType, dx :: FPType, dt :: FPType, st :: FPType, re :: FPType} deriving Show
+                            
+type MethodType = ParamsType -> Int -> Int -> [Vector FPType]
+type EulerStep = ParamsType -> Vector FPType -> Vector FPType
 
 eulerForward :: EulerStep -> MethodType
-eulerForward step tn sn = take tn (iterate step (initial sn))
+eulerForward step params tn sn = take tn (iterate (step params) (initial sn))
 
 eulerForwardAgainstFlow, eulerForwardByFlow :: MethodType
 eulerForwardByFlow = eulerForward eulerForwardByFlowStep -- #1
 eulerForwardAgainstFlow = eulerForward eulerForwardAgainstFlowStep -- #2
 
-eulerForwardStep :: (Vector Float -> Int -> Float) -> EulerStep
-eulerForwardStep gen xs = let
+type GenType = (ParamsType -> Vector FPType -> Int -> FPType)
+eulerForwardStep :: GenType -> EulerStep
+eulerForwardStep gen params xs = let
   n = V.length xs
   generator i
     | (i == 0)     = x_left
     | (i == n - 1) = x_right
-    | otherwise    = gen xs i
+    | otherwise    = gen params xs i
   in generate n generator
 
 eulerForwardByFlowStep, eulerForwardAgainstFlowStep :: EulerStep
 eulerForwardByFlowStep = eulerForwardStep gen where
-  gen xs i = 
-        re * ((xs ! i - 1) - 2 * (xs ! i) + (xs ! i + 1)) -
-        st * ((xs ! i + 1) - xs ! i) + xs ! i
+  gen :: GenType
+  gen p xs i = 
+        (re p) * ((xs ! i - 1) - 2 * (xs ! i) + (xs ! i + 1)) -
+        (st p) * ((xs ! i + 1) - xs ! i) + xs ! i
 
 eulerForwardAgainstFlowStep = eulerForwardStep gen where
-  gen xs i =
-        re * ((xs ! i - 1) - 2 * (xs ! i) + (xs ! i + 1)) -
-        st * ((xs ! i) - xs ! i - 1) + xs ! i
+  gen p xs i =
+        (re p) * ((xs ! (i - 1)) - 2 * (xs ! i) + (xs ! (i + 1))) -
+        (st p) * ((xs ! i) - (xs ! (i - 1))) + (xs ! i)
   
-myplot name xs = plot' [] X11 $ Data3D [Color Red, Style Impulses, Title name] [] xs
+defltOpts :: Graph.C graph => Opts.T graph
+defltOpts = Opts.grid True $ Opts.key False $ Opts.deflt
+
+myplot :: String -> [[(FPType, FPType, FPType)]] -> Frame.T (Graph3D.T FPType FPType FPType)
+myplot methodTitle res = Frame.cons (
+    Opts.title methodTitle $
+    Opts.xLabel "time" $
+    Opts.yLabel "space" $
+    defltOpts
+    )
+    $ Plot3D.mesh res
+
+readFPType :: String -> FPType
+readFPType = read
+
+options :: [OptDescr (Options -> Options)]
+options = [
+  Option ['k'] ["kappa"]
+  (ReqArg (\ d opts -> opts { optKappa = readFPType d }) "NUM")
+  "kappa parameter",
+  Option ['u'] []
+  (ReqArg (\ d opts -> opts { optU = readFPType d }) "NUM")  "u parameter",
+  Option ['x'] ["delta-x"]
+  (ReqArg (\ d opts -> opts { optDX = readFPType d }) "NUM") "space step",
+  Option ['t'] ["delta-t"]
+  (ReqArg (\ d opts -> opts { optDT = readFPType d }) "NUM") "time step"
+  ]
+ 
+data Options = Options
+     { optKappa :: FPType
+     , optU     :: FPType
+     , optDX    :: FPType 
+     , optDT    :: FPType 
+     } deriving Show
+defaultDT, defaultDX, defaultU, defaultKappa :: FPType
+defaultDT = 0.000001
+defaultDX = 0.1
+defaultU = 5000.0
+defaultKappa = 300.0
+
+calcSt, calcRe :: FPType -> FPType -> FPType -> FPType
+calcSt u dx dt = (u * dt) / dx
+calcRe kappa dx dt = (kappa * dt) / (dx * dx)
+defaultSt, defaultRe :: FPType
+defaultSt = calcSt defaultU defaultDX defaultDT
+defaultRe = calcRe defaultKappa defaultDX defaultDT
+
+defaultOptions :: Options
+defaultOptions = Options
+     { optKappa = defaultKappa
+     , optU = defaultU
+     , optDX = defaultDX
+     , optDT = defaultDT
+     }
+myParams :: [String] -> IO (ParamsType, [String])
+myParams argv = 
+   case getOpt Permute options argv of
+      (o,n,[]  ) -> return (fromOptions (foldl (flip id) defaultOptions o), n)
+      (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "Usage: main [OPTION...]"
+
+calcStFromParams, calcReFromParams :: ParamsType -> FPType
+calcStFromParams p = calcSt (u p) (dx p) (dt p)
+calcReFromParams p = calcRe (kappa p) (dx p) (dt p)
+recalcParams :: ParamsType -> ParamsType
+recalcParams p = p { st = calcStFromParams p, re = calcReFromParams p}
+
+fromOptions :: Options -> ParamsType
+fromOptions o = recalcParams $ Container {kappa = optKappa o, u = optU o, dx = optDX o, dt = optDT o, st = 0.0, re = 0.0}
+
 main :: IO ()
-main =
-  do
-    putStrLn $ show $ initial 200
-    putStrLn $ (printf "st = %6.3f re = %6.3f" st re)
-    writeFile "result.txt" $ join [printf "%f %f %f\n" x y z | (x, y, z) <- (ps)]
+main = do
+    argv <- getArgs
+    (o, ss) <- myParams argv
+    putStrLn $ show o
+    putStrLn $ show ss
+    _ <- GP.plotDefault(myplot "Euler explicit against flow" $ (myGroup time_steps (ps eulerForwardAgainstFlow o)))
+    putStrLn $ (printf "st = %6.3f re = %6.3f" (st o) (re o))
+    writeFile "result.txt" $ join [printf "%f %f %f\n" x y z | (x, y, z) <- (ps eulerForwardAgainstFlow o)]
     return ()
   where
-    result = eulerForwardAgainstFlow time_steps space_steps
-    ys :: Vector Float
-    gx :: Int -> Vector Float
-    gx i = V.replicate space_steps ((fromIntegral i) * dt)
-    n = space_steps - 1
-    xxs = concat $ map (V.toList . gx) (nums 0 n)
-    ys = V.iterateN space_steps (\x -> x + dx) x_left
-    yys = concat $ replicate space_steps (V.toList ys)
-    zzs = concat $ map (V.toList) result
-    ps = zip3 xxs yys zzs
+    result m p = m p time_steps space_steps
+    ys :: ParamsType -> Vector FPType
+    genTimes :: ParamsType -> Int -> Vector FPType
+    genTimes o i = V.replicate space_steps ((fromIntegral i) * (dt o))
+    n = time_steps - 1
+    xxs p = concat $ map (V.toList . (genTimes p) ) (nums 0 n)
+    ys p = V.iterateN space_steps (\x -> x + (dx p)) 0.0
+    yys p = concat $ replicate space_steps (V.toList (ys p))
+    ps m p = zip3 (xxs p) (yys p) (concat $ map (V.toList) (result m p))
+
+myGroup :: Int -> [a] -> [[a]]
+myGroup _ [] = []
+myGroup n xs = (take n xs) : (myGroup n (drop n xs))
 
 good a = and [notNaN a, notInf a]
 change a = if good a then a else 0.0
